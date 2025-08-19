3431cca007fab010b174d69cdf28474261282834
panayappan02 <panayappan02@gmail.com>
2025-08-19 08:38:09 +0530

    Routing

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 - Install Tailwind CSS
 - Install Daisy UI
 - Add NavBar component to App.jsx
+- Create a NavBar separate Component file
+- Install react-router-dom
+- Create BrowserRouter > Routes > Route = / Body > RouteChildren
+- Create an Outlet in your Body Component
+- Create a Footer
+- Create a Login Page
---
+++ src/Body.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+
+const Body = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <div className="grow">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;

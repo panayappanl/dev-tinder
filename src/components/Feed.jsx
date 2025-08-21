4080bdf30ef0db1f086569ac9e463bd82753aa70
panayappan02 <panayappan02@gmail.com>
2025-08-21 23:07:32 +0530

    built the user card on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { BASE_URL, feedSampleData } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  const getFeed = async () => {
+    try {
+      if (feed) return;
+      const res = await axios.get(`${BASE_URL}/feed`, {
+        withCredentials: true,
+      });
+      dispatch(
+        addFeed(res.data?.data?.length ? res.data?.data : feedSampleData)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed?.[0]} />
+      </div>
+    )
+  );
 };
 
 export default Feed;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,14 @@
 export const BASE_URL = "http://localhost:3000";
+
+export const feedSampleData = [
+  {
+    about: "Full-stack developer",
+    firstName: "Arjun",
+    lastName: "Reddy",
+    photoUrl:
+      "https://d2h7z5r5pp4sed.cloudfront.net/telugu/gallery/movies/arjunreddy/arjunreddy290717_01.jpg",
+    skills: ["React", "Node.js", "MongoDB", "Express"],
+    age: 28,
+    gender: "male",
+  },
+];
